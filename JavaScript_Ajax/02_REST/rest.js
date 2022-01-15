4c831a6ac95b866d6a270d246a45f31d3704324b
FynnPif <fynn.piffrader@student.htldornbirn.at>
2022-01-15 12:04:42 +0100

    02_REST fertig

---
+++ JavaScript_Ajax/02_REST/rest.js
@@ -0,0 +1,50 @@
+function registerEvents(){
+    document.getElementById("loadTodos").addEventListener("click",function(){
+        loadTodos();
+    })
+
+    document.getElementById("loadUsers").addEventListener("click",function(){
+        loadUsers();
+    })
+}
+
+function loadTodos(){
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(function(response){
+            return response.json();
+        })
+
+        .then(function(data){
+            let html = "";
+            data.forEach(todo => {
+                html += "<li>" + todo.title + "</li>";
+            });
+            document.getElementById("list").innerHTML = html;
+        })
+
+        .catch(function(err){
+            console.log(err)
+        });
+}
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(function(response){
+            return response.json();
+        })
+
+        .then(function(data){
+            let html = "";
+            data.forEach(user => {
+                html += "<li>" + user.name + "</li>";
+            });
+            document.getElementById("list").innerHTML = html;
+        })
+
+        .catch(function(err){
+            console.log(err)
+        });
+}
+
+
+registerEvents();
