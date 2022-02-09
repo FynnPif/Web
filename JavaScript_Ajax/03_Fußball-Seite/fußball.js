cd6079bf8cce059d95a8eea8861732b208350747
FynnPif <fynn.piffrader@student.htldornbirn.at>
2022-02-09 11:28:53 +0100

    Grundzüge css + spieler/teams in js

--- JavaScript_Ajax/03_Fußball-Seite/fußball.js
+++ JavaScript_Ajax/03_Fußball-Seite/fußball.js
@@ -1,16 +1,29 @@
-let urlTeams = "https://api.football-data.org/v2/competitions/2002/teams"
+let urlTeams = "https://api.football-data.org/v2/competitions/2002/teams";
+let urlPlayers = "https://api.football-data.org/v2/teams/18";
 
-fetch(urlTeams , {
-    method:"GET",
-
+fetch(urlTeams, {
     headers: {
-        "x-auth-token:": "819babcd7902454f930c154272296d78"
+        "x-auth-token": "0293f130b1874ec0b8fb31405ff7017c"
     }
 })
 .then(repsonse => repsonse.json())
 .then(function(data) {
     let html = ""
     data.teams.forEach(element => {
+        html += "<li>" + element.name + "</li>"        
+    });
+    document.getElementById("team##s").innerHTML = html;
+})
+
+fetch(urlPlayers, {
+    headers: {
+        "x-auth-token": "0293f130b1874ec0b8fb31405ff7017c"
+    }
+})
+.then(repsonse => repsonse.json())
+.then(function(data) {
+    let html = ""
+    data.squad.forEach(element => {
         html += "<li>" + element.name + "</li>"        
     });
     document.getElementById("teams").innerHTML = html;
