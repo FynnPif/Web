c2996e82a36ef68653b3810ddd172245886e2b40
FynnPif <fynn.piffrader@student.htldornbirn.at>
2022-02-23 10:48:37 +0100

    einfügen von json in website

--- JavaScript_Ajax/03_Fußball-Seite/fußball.js
+++ JavaScript_Ajax/03_Fußball-Seite/fußball.js
@@ -1,30 +1,52 @@
 let urlTeams = "https://api.football-data.org/v2/competitions/2002/teams";
 let urlPlayers = "https://api.football-data.org/v2/teams/18";
+let urlLeagues = "https://api.football-data.org/v2/competitions";
 
-fetch(urlTeams, {
-    headers: {
-        "x-auth-token": "0293f130b1874ec0b8fb31405ff7017c"
-    }
-})
-.then(repsonse => repsonse.json())
-.then(function(data) {
-    let html = ""
-    data.teams.forEach(element => {
-        html += "<li>" + element.name + "</li>"        
-    });
-    document.getElementById("team##s").innerHTML = html;
-})
+function teams(){
+    fetch(urlTeams, {
+        headers: {
+            "x-auth-token": "0293f130b1874ec0b8fb31405ff7017c"
+        }
+    })
+    .then(repsonse => repsonse.json())
+    .then(function(data) {
+        let html = ""
+        data.teams.forEach(element => {
+            html += "<li> <button>" + element.name + "</button> </li>"        
+        });
+        document.getElementById("content").innerHTML = html;
+    })
+}
 
-fetch(urlPlayers, {
-    headers: {
-        "x-auth-token": "0293f130b1874ec0b8fb31405ff7017c"
-    }
-})
-.then(repsonse => repsonse.json())
-.then(function(data) {
-    let html = ""
-    data.squad.forEach(element => {
-        html += "<li>" + element.name + "</li>"        
-    });
-    document.getElementById("teams").innerHTML = html;
-})
+
+function players(){
+    fetch(urlPlayers, {
+        headers: {
+            "x-auth-token": "0293f130b1874ec0b8fb31405ff7017c"
+        }
+    })
+    .then(repsonse => repsonse.json())
+    .then(function(data) {
+        let html = ""
+        data.squad.forEach(element => {
+            html += "<li>" + element.name + "</li>"        
+        });
+        document.getElementById("content").innerHTML = html;
+    })
+}
+
+function leagues(){
+    fetch(urlLeagues, {
+        headers: {
+            "x-auth-token": "0293f130b1874ec0b8fb31405ff7017c"
+        }
+    })
+    .then(repsonse => repsonse.json())
+    .then(function(data) {
+        let html = ""
+        data.competitions.forEach(element => {
+            html += "<li id='list'>" + element.name + "</li>"        
+        });
+        document.getElementById("content").innerHTML = html;
+    })
+}
