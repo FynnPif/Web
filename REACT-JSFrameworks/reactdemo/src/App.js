6f65c1628758adb2f0ead6fbdae13ec360e01382
FynnPif <fynn.piffrader@student.htldornbirn.at>
2022-03-02 11:07:14 +0100

    04 React First Component fertig

---
+++ REACT-JSFrameworks/reactdemo/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SuperButton from './components/SuperButton';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <SuperButton/>
+    </div>
+  );
+}
+
+export default App;
