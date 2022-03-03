bad770d2f22f599f74d7f9f9651f0c045c6b7377
FynnPif <fynn.piffrader@student.htldornbirn.at>
2022-03-03 10:31:24 +0100

    05 react styling fertig

--- REACT-JSFrameworks/reactdemo/src/App.js
+++ REACT-JSFrameworks/reactdemo/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import ButtonRow from './components/ButtonRow';
 import SuperButton from './components/SuperButton';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 class="heading">Hello</h1>
       <SuperButton/>
+      <ButtonRow/>
+      <ButtonRow/>
+      <ButtonRow/>
     </div>
   );
 }
--- REACT-JSFrameworks/reactdemo/src/components/SuperButton.js
+++ REACT-JSFrameworks/reactdemo/src/components/SuperButton.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-
+import styles from './SuperButton.module.css';
 export default class SuperButton extends Component{
     render(){
-        return <div>
+        return (<div className={styles.heading}>
             <button>Button</button>
-        </div>
+        </div>)
     }
 }
