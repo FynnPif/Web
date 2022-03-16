0a8966ffe371ceebdce0acd7d6fa89cfd6d47d8b
FynnPif <fynn.piffrader@student.htldornbirn.at>
2022-03-16 11:09:52 +0100

    08_Functional Components

--- REACT-JSFrameworks/reactdemo/src/App.js
+++ REACT-JSFrameworks/reactdemo/src/App.js
@@ -1,27 +1,34 @@
 import './App.css';
+import Counter from './components/Counter';
+import CounterFunc from './components/CounterFunc';
 import Vote from './components/Vote';
-{/*import ButtonRow from './components/ButtonRow';
+import ButtonRow from './components/ButtonRow';
 import SuperButton from './components/SuperButton';
 import ButtonList from './components/ButtonList';
-import Card from './components/Card';*/}
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <h1 class="heading">Hello</h1>
-      {/*<SuperButton/>
+      
+      <Counter/>
+      <CounterFunc/>
+
+      {/*<h1 class="heading">Hello</h1>
+      <SuperButton/>
       <ButtonRow/>
       <ButtonRow/>
       <ButtonRow/>
       <ButtonList/>
-      <Card/>*/}
+      <Card/>
       <div class="container">
         <Vote candidate="Sepp" address="Dornbirn"/>
         <Vote candidate="Paul" address="Bregenz"/>
         <Vote candidate="Hans" address="Feldkirch"/>
-      </div>
+  </div>*/}
     </div>
   );
 }
 
 export default App;
+
